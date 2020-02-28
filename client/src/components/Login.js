8699d512e70877053cb81bceb0ec68bd59d3493c
Josue Rodriguez <jr2589213@gmail.com>
2020-02-28 09:45:37 -0800

    Create form, validate user via token in local storage, and map/display on screen
    * Added form that holds users username and password
    
    * Created axiosWithAuth and PrivateRoute functions under untils folder
    
    * Authorized user login by posting users login credentials to server and setting token to local storage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import {useHistory} from 'react-router-dom'
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [userInput, setUserInput] = useState({
+    username: '',
+    password: ''
+  })
+
+  //Need to redirect after user submits !!!!!!!!!!!!!
+  let history = useHistory()
+
+  console.log(userInput)
+
+  const handleChange = (e) => {
+    setUserInput({...userInput, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axiosWithAuth().post('/api/login', userInput)
+    .then(res => {
+      window.localStorage.setItem('token', res.data.payload)
+      history.push('/profile')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>username</label>
+        <input name='username' onChange={handleChange} value={userInput.username}/>
+
+        <label htmlFor='password'>password</label>
+        <input name='password' onChange={handleChange} value={userInput.password}/>
+
+        <button type='submit'>Submit</button>
+      </form>
     </>
   );
 };
